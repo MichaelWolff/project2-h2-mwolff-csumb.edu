48224d8bd0f5d0782fe29c09b3561bcb2cead95b
Michael Wolff <mwolff@csumb.edu>
2017-03-06 15:30:47 +0000

    Made Alfred the chat bot

--- app.py
+++ app.py
@@ -162,9 +162,9 @@ def parseWithAlfred(message):
         if message[2:]=="help":
             print "Found help command"
             text = "Yes, Master Bruce? !!help - A list of commands you can enter !!say <something> - Make me say something  !!about - Description about this page  !!Batsy - I will say something batsy  !!Joker - I will tell a joke"
-        else:
-            print "Command not recognized"
-            text = "I'm sorry Master Bruce but I do not recognize your command"
+        #else:
+         #   print "Command not recognized"
+          #  text = "I'm sorry Master Bruce but I do not recognize your command"
     if message[0:2]==">>":
         print "Alfred read that a user logged in"
         text = "Welcome "+message[2:]+"! Type !!help for a list of commands!"
