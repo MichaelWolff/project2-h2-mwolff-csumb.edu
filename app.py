b0e81c11da5418832bb7a4b587798b946d977bc2
Michael Wolff <mwolff@csumb.edu>
2017-03-06 06:37:34 +0000

    Starting the postgresql service again

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ def getUserIndex(username):
     return -1
 
 if __name__ == '__main__':
-    socketio.run(app,host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',8080)),debug=True)
+    socketio.run(app,host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT',5432)),debug=True)
